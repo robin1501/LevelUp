97591d193c0401038ff363c3d80a36885ce231c0
Yannickh91 <yannickhaas91@gmail.com>
2014-11-19 12:08:49 +0100

    Einstellungen Layout

--- app/src/main/java/com/example/krro/levelup/ProfilActivity.java
+++ app/src/main/java/com/example/krro/levelup/ProfilActivity.java
@@ -1,13 +1,27 @@
 package com.example.krro.levelup;
 
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfilActivity extends Activity {
+
+    private DBHelper dbHelper = new DBHelper(getApplication());
+    private SQLiteDatabase db = dbHelper.getWritableDatabase();
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.profil);
+       // Button save = (Button)findViewById(R.id.angry_btn);
+        //save.setOnClickListener(new View.OnClickListener() {
+          //  @Override
+            //public void onClick(View v) {
+              //  db.update("profil",);
+         //   }
+       // });
 	}
 }
