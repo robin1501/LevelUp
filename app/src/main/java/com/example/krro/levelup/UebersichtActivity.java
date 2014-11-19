7e12aefb5acf7d91770d90f840623b59aa22a59a
PatrickWei <pa.wei@web.de>
2014-11-19 12:42:09 +0100

    GRAAAAFIK

--- app/src/main/java/com/example/krro/levelup/UebersichtActivity.java
+++ app/src/main/java/com/example/krro/levelup/UebersichtActivity.java
@@ -1,46 +1,108 @@
 package com.example.krro.levelup;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
+//import android.content.Context;
+//import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+//import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
 import org.achartengine.model.CategorySeries;
+import org.achartengine.model.SeriesSelection;
 import org.achartengine.renderer.DefaultRenderer;
 import org.achartengine.renderer.SimpleSeriesRenderer;
 
 public class UebersichtActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.uebersicht);
-	}
 
-    public Intent getIntent(Context context) {
+    private static int[] COLORS = new int[] { Color.GREEN, Color.BLUE,Color.MAGENTA, Color.CYAN };
 
-        int[] values  = {1 ,2 ,3 ,4 ,5};
+    private static double[] VALUES = new double[] { 10, 11, 12, 13 };
 
-        CategorySeries series = new CategorySeries("Pie Graph");
-        int k= 0;
-        for (int value : values){
-            series.add("Section" + ++k, + value);
+    private static String[] NAME_LIST = new String[] { "A", "B", "C", "D" };
+
+    private CategorySeries mSeries = new CategorySeries("");
+
+    private DefaultRenderer mRenderer = new DefaultRenderer();
+
+    private GraphicalView mChartView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.uebersicht);
+
+        mRenderer.setApplyBackgroundColor(true);
+        //mRenderer.setBackgroundColor(Color.argb(100, 50, 50, 50));
+        mRenderer.setChartTitleTextSize(20);
+        mRenderer.setLabelsTextSize(20);
+        mRenderer.setLegendTextSize(20);
+        mRenderer.setPanEnabled(false);
+       // mRenderer.setMargins(new int[] { 20, 30, 15, 0 });
+        mRenderer.setLegendHeight(-20);
+        mRenderer.setZoomButtonsVisible(false);
+        mRenderer.setStartAngle(90);
+
+        for (int i = 0; i < VALUES.length; i++) {
+            mSeries.add(NAME_LIST[i] + " " + VALUES[i], VALUES[i]);
+            SimpleSeriesRenderer renderer = new SimpleSeriesRenderer();
+            renderer.setColor(COLORS[(mSeries.getItemCount() - 1) % COLORS.length]);
+            mRenderer.addSeriesRenderer(renderer);
         }
 
-        int[] colors =  new int[] {Color.BLUE, Color.RED, Color.GREEN, Color.YELLOW, Color.CYAN};
-
-        DefaultRenderer renderer = new DefaultRenderer();
-        for (int color: colors){
-            SimpleSeriesRenderer r = new SimpleSeriesRenderer();
-            r.setColor(color);
-            renderer.addSeriesRenderer(r);
+        if (mChartView != null) {
+            mChartView.repaint();
         }
 
-        Intent intent = ChartFactory.getPieChartIntent(context, series, renderer, "Pie");
+    }
 
-        return intent;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mChartView == null) {
+            LinearLayout layout = (LinearLayout) findViewById(R.id.chart);
+            mChartView = ChartFactory.getPieChartView(this, mSeries, mRenderer);
+            mRenderer.setClickEnabled(true);
+            mRenderer.setSelectableBuffer(10);
 
+            mChartView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    SeriesSelection seriesSelection = mChartView.getCurrentSeriesAndPoint();
+
+                    if (seriesSelection == null) {
+                        Toast.makeText(UebersichtActivity.this, "No chart element was clicked", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(UebersichtActivity.this,"Chart element data point index "+ (seriesSelection.getPointIndex()+1) + " was clicked" + " point value="+ seriesSelection.getValue(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+            mChartView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    SeriesSelection seriesSelection = mChartView.getCurrentSeriesAndPoint();
+                    if (seriesSelection == null) {
+                        Toast.makeText(UebersichtActivity.this,"No chart element was long pressed", Toast.LENGTH_SHORT);
+                        return false;
+                    }
+                    else {
+                        Toast.makeText(UebersichtActivity.this,"Chart element data point index "+ seriesSelection.getPointIndex()+ " was long pressed",Toast.LENGTH_SHORT);
+                        return true;
+                    }
+                }
+            });
+            layout.addView(mChartView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+        }
+        else {
+            mChartView.repaint();
+        }
     }
 }
