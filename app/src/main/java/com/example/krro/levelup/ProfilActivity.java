7fa408fbca45f0ffa2ecaacfdfbfeebba87325d7
PatrickWei <pa.wei@web.de>
2014-11-26 16:41:10 +0100

    Profil DB

--- app/src/main/java/com/example/krro/levelup/ProfilActivity.java
+++ app/src/main/java/com/example/krro/levelup/ProfilActivity.java
@@ -1,6 +1,7 @@
 package com.example.krro.levelup;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -8,20 +9,28 @@ import android.widget.Button;
 
 public class ProfilActivity extends Activity {
 
-   // private DBHelper dbHelper = new DBHelper(getApplication());
-    //private SQLiteDatabase db = dbHelper.getWritableDatabase();
+    private DBHelper dbHelper = new DBHelper(getApplication());
+    private SQLiteDatabase db = dbHelper.getWritableDatabase();
+
 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.profil);
-       // Button save = (Button)findViewById(R.id.angry_btn);
-        //save.setOnClickListener(new View.OnClickListener() {
-          //  @Override
-            //public void onClick(View v) {
-              //  db.update("profil",);
-         //   }
-       // });
+        Button save = (Button)findViewById(R.id.angry_btn);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+           public void onClick(View v) {
+                ContentValues args = new ContentValues();
+                args.put("name", R.id.editTextName);
+                args.put("age", R.id.editTextAlter);
+                args.put("gewicht", R.id.editTextGewicht);
+                args.put("groesse", R.id.editTextGröße);
+                //args.put("geschlecht", R.id.editTextGeschlecht);
+                args.put("wunschgewicht", R.id.editTextWunschgewicht);
+                db.update("profil",args,"p_id", null );
+            }
+        });
 	}
 }
