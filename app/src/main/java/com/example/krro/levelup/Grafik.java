90d3d009743ad4e8f3553952da49dfaa9d9d9cfa
PatrickWei <pa.wei@web.de>
2014-12-30 18:27:48 +0100

    CURSOR

--- app/src/main/java/com/example/krro/levelup/Grafik.java
+++ app/src/main/java/com/example/krro/levelup/Grafik.java
@@ -1,8 +1,13 @@
 package com.example.krro.levelup;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -21,7 +26,11 @@ import org.achartengine.renderer.XYSeriesRenderer;
  */
 
 
+
 public class Grafik extends Activity {
+    private DBHelper dbHelper;
+    private SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +38,24 @@ public class Grafik extends Activity {
 
         OpenChart();
 
+        int id;
+        Intent uebungDetail = getIntent();
+        id = uebungDetail.getIntExtra("id", 0);
+        dbHelper = new DBHelper(getApplicationContext());
+        db = dbHelper.getReadableDatabase();
+
+     //   String query = "SELECT gewicht FROM todo_workoutpos WHERE u_id = " + 0;
+        String query = "SELECT tp.gewicht, th.datum FROM todo_workoutpos tp inner join todo_workouthead th on tp.t_id = th.t_id WHERE u_id = " + id;
+
+        Cursor cursor = db.rawQuery(query, null);
+        Log.i("Number of Records"," :: "+cursor.getCount());
+
+        if (cursor.getCount() != 0) {
+            cursor.moveToFirst();
+
+            double gewicht = cursor.getDouble(0);
+            Log.i("Number of Records 1"," :: "+cursor.getCount());
+        }
     }
     private GraphicalView mChart;
 
@@ -37,11 +64,11 @@ public class Grafik extends Activity {
             "Jan", "Feb" , "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Okt",
     "Nov", "Dez"};
 
-
     private void OpenChart()
     {
      // Define the number of elements you want in the chart.
         int z[]={0,1,2,3,4,5,6,7};
+
 
 
         int x[]={10,18,32,21,48,60,53,60};
